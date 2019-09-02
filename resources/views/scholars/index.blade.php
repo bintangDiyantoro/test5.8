f558d3dca3da94c2828f9548f764b2e915d83a3f
Bintang Diyantoro <bintangdiyantoro@gmail.com>
2019-09-02 14:04:39 +0700

    CRUD done

--- resources/views/scholars/index.blade.php
+++ resources/views/scholars/index.blade.php
@@ -2,33 +2,39 @@
 @section('title', 'Scholar Page')
 @section('nav2', 'active')
 @section('content')
-<h1>Scholars List</h1>
-<table class="table table-hover mt-3">
-    <thead>
-        <tr>
-            <th scope="col">#</th>
-            <th scope="col">Name</th>
-            <th scope="col">NIS</th>
-            <th scope="col">Email</th>
-            <th scope="col">Department</th>
-            <th scope="col" class="text-center">Action</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($scholars as $scholar)
-        <tr>
-            <th scope="row">{{$loop->iteration}}</th>
-            <td>{{ $scholar->name}}</td>
-            <td>{{ $scholar->nis}}</td>
-            <td>{{ $scholar->email}}</td>
-            <td>{{ $scholar->department}}</td>
-            <td class="d-flex justify-content-center">
-                <a href="#" class="badge badge-pill badge-info">Detail</a>
-                <a href="#" class="badge badge-pill badge-warning">Edit</a>
-                <a href="#" class="badge badge-pill badge-danger">Delete</a>
-            </td>
-        </tr>
-        @endforeach
-    </tbody>
-</table>
+<div class="container mt-4">
+    <div class="row">
+        <div class="col-md-10">
+            <h1>Scholars List</h1>
+            <table class="table table-hover mt-3">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">NIS</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Department</th>
+                        <th scope="col" class="text-center">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($scholars as $scholar)
+                    <tr>
+                        <th scope="row">{{$loop->iteration}}</th>
+                        <td>{{ $scholar->name}}</td>
+                        <td>{{ $scholar->nis}}</td>
+                        <td>{{ $scholar->email}}</td>
+                        <td>{{ $scholar->department}}</td>
+                        <td class="d-flex justify-content-center">
+                            <a href="#" class="badge badge-pill badge-info">Detail</a>
+                            <a href="#" class="badge badge-pill badge-warning">Edit</a>
+                            <a href="#" class="badge badge-pill badge-danger">Delete</a>
+                        </td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
 @endsection
--- resources/views/students/detail.blade.php
+++ resources/views/students/detail.blade.php
@@ -2,21 +2,33 @@
 @section('title', $student->name)
 @section('nav3', 'active')
 @section('content')
-<div class="card col-sm-5">
-    <div class="card-body">
-        <h6 class="card-subtitle mb-2 text-muted text-center">Student's detail</h6>
-        <div class="float-left mr-3">
-            <h5 class="card-title">Name</h5>
-            <p class="card-text">NIS</p>
-            <p class="card-text">Email</p>
-            <p class="card-text">Department</p>
-        </div>
-        <div class="float-center">
-            <h5 class="card-title">: {{$student->name}}</h5>
-            <p class="card-text">: {{$student->nis}}</p>
-            <p class="card-text">: {{$student->email}}</p>
-            <p class="card-text">: {{$student->department}}</p>
-            <a href="{{url('students')}}" class="btn btn-primary float-right">Go Back</a>
+<div class="container mt-4">
+    <div class="row">
+        <div class="col-md-10">
+            <div class="card col-sm-5">
+                <div class="card-body">
+                    <h3 class="card-subtitle mb-2 text-muted text-center">Student's detail</h3>
+                    <div class="float-left mr-3">
+                        <h5 class="card-title">Name</h5>
+                        <p class="card-text">NIS</p>
+                        <p class="card-text">Email</p>
+                        <p class="card-text">Department</p>
+                    </div>
+                    <div class="float-center">
+                        <h5 class="card-title">: {{$student->name}}</h5>
+                        <p class="card-text">: {{$student->nis}}</p>
+                        <p class="card-text">: {{$student->email}}</p>
+                        <p class="card-text">: {{$student->department}}</p>
+                        <a href="{{url('students/'.$student->id.'/edit')}}" class="btn btn-warning">Edit</a>
+                        <form action="{{url('students/'.$student->id)}}" method="post" class="d-inline">
+                            @method('DELETE')
+                            @csrf
+                            <button type="submit" class="btn btn-danger" onclick="return confirm('Are sure want to delete {{$student->name}}?')">Delete</button>
+                        </form>
+                        <a href="{{url('students')}}" class="btn btn-primary float-right">Go Back</a>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
